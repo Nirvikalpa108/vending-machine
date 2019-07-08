4f2ded8d613a12f7015901bfb7a19a1e0439413c
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-08 20:03:49 +0100

    updates initial programme message

--- spec/snack_machine_spec.rb
+++ spec/snack_machine_spec.rb
@@ -1,8 +1,13 @@
 require_relative '../vending_machine'
 
 describe 'Vending machine' do
-  it 'should print "Snickers: £1, Bounty: £1.50" to the console' do
-    expect { Purchase.new }.to output("Snickers: £1, Bounty: £1.50").to_stdout do
+  it 'should print opening message to the console' do
+    expect { Purchase.new }.to output("Snickers: £1, Bounty: £1.50. Which snack would you like to buy? (Please note that no change is given).").to_stdout do
     end
   end
 end
+
+# User prompted to type snack choice
+# Programme asks them to enter their money (and tells them how to type it)
+# User enters money
+# Machine calculates how much they have left to pay until their oustanding balance is zero or less
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,6 +1,6 @@
 class Purchase
 
   def initialize
-    print "Snickers: £1, Bounty: £1.50"
+    print "Snickers: £1, Bounty: £1.50. Which snack would you like to buy? (Please note that no change is given)."
   end
 end

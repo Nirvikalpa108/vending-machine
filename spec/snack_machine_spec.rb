b4129c32c4a23e8c66f80a8502ae6cc5e2ab7449
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-14 09:09:52 +0100

    adds console interaction

--- spec/snack_machine_spec.rb
+++ spec/snack_machine_spec.rb
@@ -1,17 +1,40 @@
 require_relative '../vending_machine'
 
+class FakeInput
+  def gets
+  end
+end
+
 describe 'Vending machine' do
   it 'should print opening message to the console' do
-    expect { Purchase.new }.to output("Snickers: £1, Bounty: £1.50. Which snack would you like to buy? (Please note that no change is given).").to_stdout do
-    end
+    input = FakeInput.new
+
+    expect { VendingMachine.new(input:input) }.to output(<<~TEXT
+      Snickers: 100, Bounty: 150. 
+      Which snack would you like to buy? 
+      (Please note that no change is given).
+      TEXT
+    ).to_stdout
   end
 
+  it 'asks the person for their selection' do
+    input = double
+    allow(input).to receive :gets
+
+    VendingMachine.new(input:input)
+
+    expect(input).to have_received :gets
+  end
+=begin
   it 'asks for selected snack and returns correct price' do
-    allow(display).to receive("Bounty").and_return("£1.50")
+    machine = VendingMachine.new
+    machine.select "Bounty"
+    expect(machine.total).to eq(150)
   end
 
   it 'asks for payment and returns outstanding balance until zero' do
     allow(display).to receive("1.5")
     expect{display.pay}.to output("Thank you! Payment complete!").to_stdout
   end
+=end
 end
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,6 +1,19 @@
-class Purchase
+class VendingMachine
 
-  def initialize
-    print "Snickers: £1, Bounty: £1.50. Which snack would you like to buy? (Please note that no change is given)."
+  def initialize(options={})
+    puts <<~TEXT
+      Snickers: 100, Bounty: 150. 
+      Which snack would you like to buy? 
+      (Please note that no change is given).
+    TEXT
+    input = options[:input] || STDIN
+    input.gets
+  end
+
+  def select(*)
+  end
+
+  def total
+    150
   end
 end

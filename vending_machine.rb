d44a39df4a816e0f02ed4f4368935704fd4962f5
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-14 14:13:49 +0100

    adds till, sell and change methods

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,11 +1,27 @@
 class VendingMachine
 
   SNACKS = { "Bounty" => 100, "Twirl" => 150, "Wispa" => 200, "Maltesers" => 170 }
+  TILL = { 100 => 30, 50 => 40, 20 => 50, 10 => 80 }
 
   def snacks
     SNACKS
   end
 
+  def till
+    TILL
+  end
 
+  def sell(selection, payment)
+    payment - SNACKS[selection]
+  end
+
+  def change(selection, payment)
+    balance = sell(selection, payment)
+    if balance >= 1 && TILL.key?(balance)
+      # do value - 1 for TILL[balance]
+    else
+      puts "no exact change available"
+    end
+  end
 end
 

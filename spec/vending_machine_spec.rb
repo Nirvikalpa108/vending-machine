18bd3b89fd371892e4e8454a7d67d6a68c4bb0b7
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-14 14:13:38 +0100

    renames tests

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -2,21 +2,32 @@ require 'rspec'
 require_relative '../vending_machine'
 
 describe 'Vending machine' do
-  it 'displays list of products and prices' do
+  it 'returns correct price upon product selection' do
     expect(VendingMachine.new.snacks["Bounty"]).to eq(100)
     expect(VendingMachine.new.snacks["Twirl"]).to eq(150)
     expect(VendingMachine.new.snacks["Wispa"]).to eq(200)
     expect(VendingMachine.new.snacks["Maltesers"]).to eq(170)
   end
 
-=begin
-  it 'displays the list of money denominations available' do
+  it 'returns balance upon payment' do
+    expect(VendingMachine.new.sell("Bounty", 100)).to eq(0)
+    expect(VendingMachine.new.sell("Twirl", 150)).to eq(0)
+    expect(VendingMachine.new.sell("Bounty", 50)).to eq(-50)
+    expect(VendingMachine.new.sell("Twirl", 100)).to eq(-50)
+    expect(VendingMachine.new.sell("Bounty", 200)).to eq(100)
+    expect(VendingMachine.new.sell("Maltesers", 200)).to eq(30)
   end
 
-  it 'returns correct price upon product selection' do
+  it 'displays the change available' do
+    expect(VendingMachine.new.till[100]).to eq(30)
+    expect(VendingMachine.new.till[50]).to eq(40)
+    expect(VendingMachine.new.till[20]).to eq(50)
+    expect(VendingMachine.new.till[10]).to eq(80)
   end
 
-  it 'returns outstanding balance until zero upon payment' do
+  it 'returns change' do
+    total = VendingMachine::TILL[VendingMachine::SNACKS["Bounty"]]
+    expect(VendingMachine::TILL[VendingMachine.new.change("Bounty", 200)])
+        .to eq(total - 1)
   end
-=end
 end

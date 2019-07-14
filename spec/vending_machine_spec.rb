dab1aefb0285d68c8bd0497355671803814a7567
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-14 23:10:41 +0100

    refactors methods
    Here we have extracted out a number of methods, attempting to improve the business logic and readability of the code. The tests have also significantly changed as part of this refactoring.

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,36 +1,19 @@
 require 'rspec'
 require_relative '../vending_machine'
 
+RSpec.describe 'Vending machine sells snack successfully' do
+  specify { expect {(VendingMachine.new.sell("Bounty", 100))}.to output("Enjoy your purchase! Thank you!\n").to_stdout }
+  specify { expect {(VendingMachine.new.sell("Freddo", 100))}.to output("Here is your 50p, thank you for your purchase!\n").to_stdout }
+  specify { expect {(VendingMachine.new.sell("Maltesers", 200))}.to output("No exact change available.\n").to_stdout }
+end
+
 describe 'Vending machine' do
-  it 'returns correct price upon product selection' do
-    expect(VendingMachine.new.snacks["Bounty"]).to eq(100)
-    expect(VendingMachine.new.snacks["Twirl"]).to eq(150)
-    expect(VendingMachine.new.snacks["Wispa"]).to eq(200)
-    expect(VendingMachine.new.snacks["Maltesers"]).to eq(170)
+  it 'adds payment to till inventory' do
+    expect(VendingMachine.new.send(:till_add, 10)).to eq(VendingMachine::TILL[10] = 81)
   end
 
-  it 'returns balance upon payment' do
-    expect(VendingMachine.new.sell("Bounty", 100)).to eq(0)
-    expect(VendingMachine.new.sell("Twirl", 150)).to eq(0)
-    expect(VendingMachine.new.sell("Bounty", 50)).to eq(-50)
-    expect(VendingMachine.new.sell("Twirl", 100)).to eq(-50)
-    expect(VendingMachine.new.sell("Bounty", 200)).to eq(100)
-    expect(VendingMachine.new.sell("Maltesers", 200)).to eq(30)
-  end
-
-  it 'displays the change available' do
-    expect(VendingMachine.new.till[100]).to eq(30)
-    expect(VendingMachine.new.till[50]).to eq(40)
-    expect(VendingMachine.new.till[20]).to eq(50)
-    expect(VendingMachine.new.till[10]).to eq(80)
-  end
-
-  it 'returns change' do
-    new_total = VendingMachine.new.change("Bounty", 200)
-    expect(new_total).to eq(29)
-  end
-
-  it 'returns "no exact change available" if exact change not given' do
-    expect(VendingMachine.new.change("Maltesers", 200)).to eq("no exact change available")
+  it 'removes change from till inventory' do
+    expect(VendingMachine.new.send(:till_remove, "Freddo", 70)).to eq(VendingMachine::TILL[20] = 49)
   end
 end
+
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,7 +1,15 @@
 class VendingMachine
 
-  SNACKS = { "Bounty" => 100, "Twirl" => 150, "Wispa" => 200, "Maltesers" => 170 }
-  TILL = { 100 => 30, 50 => 40, 20 => 50, 10 => 80 }
+  SNACKS = { "Bounty" => 100, "Freddo" => 50, "Wispa" => 200, "Maltesers" => 170 }
+  TILL = { 200 => 0, 100 => 30, 50 => 40, 20 => 50, 10 => 80 }
+
+  def sell(snack, payment)
+    till_add(payment)
+    give_change(change(snack, payment), snack, payment)
+    till_remove(snack, payment)
+  end
+
+  private
 
   def snacks
     SNACKS
@@ -11,17 +19,33 @@ class VendingMachine
     TILL
   end
 
-  def sell(selection, payment)
-    payment - SNACKS[selection]
+  def give_change(amount, snack, payment)
+    if amount > 0 && valid_change?(snack, payment)
+      puts "Here is your #{amount}p, thank you for your purchase!"
+    elsif amount == 0
+      puts "Enjoy your purchase! Thank you!"
+    else
+      puts "No exact change available."
+    end
   end
 
-  def change(selection, payment)
-    balance = sell(selection, payment)
-    if balance >= 1 && TILL.key?(balance)
-      TILL[balance] -= 1
-    else
-      "no exact change available"
+  def change(snack, payment)
+    price = SNACKS[snack]
+    payment - price
+  end
+
+  def valid_change?(snack, payment)
+    balance = change(snack, payment)
+    balance > 0 && TILL.key?(balance)
+  end
+
+  def till_add(payment)
+    TILL[payment] += 1
+  end
+
+  def till_remove(snack, payment)
+    if valid_change?(snack, payment)
+    TILL[change(snack, payment)] -= 1
     end
   end
 end
-
